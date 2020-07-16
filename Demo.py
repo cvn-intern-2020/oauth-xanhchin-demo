13ea43cddc8adc6bee86161a2982b33f45dc52f1
tranhlok <tranhlok@gmail.com>
2020-07-16 09:45:36 +0700

    fix

--- Demo.py
+++ Demo.py
@@ -26,11 +26,11 @@ resp = requests.post(url, data=bodyparams)
 creds = resp.json()
 print("Initial access token and refresh token:",resp.json())
 
-item = 'AQD91c6vyO5gTlO1lYUBlu_COiWtOzNZ5nlCxyvEuxb5_O6g-phehcflEbp5t6IU5yhfcfLPMbXubi3Nv_bQrfRK2iKVJOhCXKy4jxgPvG_XyL5VSbtpV9E93C_Vvit1kiQ'
+refreshToken = 'AQD91c6vyO5gTlO1lYUBlu_COiWtOzNZ5nlCxyvEuxb5_O6g-phehcflEbp5t6IU5yhfcfLPMbXubi3Nv_bQrfRK2iKVJOhCXKy4jxgPvG_XyL5VSbtpV9E93C_Vvit1kiQ'
 url1 = 'https://accounts.spotify.com/api/token'
 bodyparams = {'client_id':'95da2990eb16408a96a93cfa3f27555c','client_secret':'86caad313351402fb28adc216fe6a44d','grant_type': 'refresh_token', 'refresh_token': '','redirect_uri':'https://localhost/callback/','scope': 'user-follow-read user-top-read user-read-recently-played user-library-read'}
 
-bodyparams['refresh_token'] = item
+bodyparams['refresh_token'] = refreshToken
 
 resp2 = requests.post(url1, data=bodyparams)
 newAccess = resp2.json()['access_token']
@@ -41,12 +41,21 @@ headerAccess = {"Authorization": "Bearer " + newAccess}
 resp3 = requests.get(url, headers = headerAccess)
 print (resp3.json())
 
+with open('followingArtists.json', 'w', encoding='utf-8') as f1:
+    json.dump(resp3.json(), f1, ensure_ascii=False, indent=4)
+
 url1 = 'https://api.spotify.com/v1/me/tracks'
 headerAccess = {"Authorization": "Bearer " + newAccess}
 resp4 = requests.get(url1, headers = headerAccess)
 print (resp4.json())
 
+with open('savedTracks.json', 'w', encoding='utf-8') as f2:
+    json.dump(resp4.json(), f2, ensure_ascii=False, indent=4)
+
 url2 = 'https://api.spotify.com/v1/me/top/tracks'
 headerAccess = {"Authorization": "Bearer " + newAccess}
 resp5 = requests.get(url2, headers = headerAccess)
 print (resp5.json())
+
+with open('topTracks.json', 'w', encoding='utf-8') as f3:
+    json.dump(resp5.json(), f3, ensure_ascii=False, indent=4)
